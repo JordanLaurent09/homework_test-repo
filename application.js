b485c9b666567526d4639330dbd6ca965399f885
JordanLaurent09 <bluff_eversmoking@inbox.ru>
2025-04-08 17:32:13 +0300

    create add, divide, substract functions

--- application.js
+++ application.js
@@ -2,4 +2,17 @@ function multiplyTwoNumbers( numberOne, numberTwo ) {
     return numberOne * numberTwo;
 }
 
+function addTwoNumbers( numberOne, numberTwo ) {
+    return numberOne + numberTwo;
+}
+
+function divideTwoNumbers( numberOne, numberTwo ) {
+    if (numberTwo === 0) throw new Error('Divide by zero');
+    return numberOne / numberTwo;
+}
+
+function substractTwoNumbers( numberOne, numberTwo ) {
+    return numberOne - numberTwo;
+}
+
 console.log(multiplyTwoNumbers(3, 7));
